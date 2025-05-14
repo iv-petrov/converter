2bbd3a21f7cd8beeb2e1ec705b10d3d75f5495ed
Петров Игорь <igvapetrov@inno.tech>
2025-05-14 10:19:25 +0300

    Исправлены замечания

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { useState } from 'react';
 import MyCurrency from './Components/MyCurrency';
 
 function App() {
-  const [fromCurrency, setFrom] = useState(' '); 
-  const [toCurrency, setTo] = useState(' ');
+  const [fromCurrency, setFrom] = useState(0); 
+  const [toCurrency, setTo] = useState(0);
   const [amount, setAmount] = useState(0.0);
-  const [result, SetResult] = useState(0.0);
+  const [result, setResult] = useState(0.0);
 
-  function CalcResult() {
+  function calcResult() {
     if (fromCurrency === "0") {
       alert("Не выбрана валюта для конвертации");
       return;
@@ -17,31 +17,32 @@ function App() {
       alert("Не выбрана валюта, в которую конвертируем");
       return;
     }
-    if (amount === "0" || amount === "") {
-      alert("Не задано количество конвертируемой валюты");
+    if (Math.fround(amount) <= 0) {
+      alert("Задано неверное количество конвертируемой валюты");
       return;
     }
-    SetResult(Math.round(Math.abs(amount) * Math.abs(fromCurrency) / Math.abs(toCurrency) * 100) / 100);
-    return;
+    setResult(Math.round(Math.fround(amount) * Math.fround(fromCurrency) / Math.fround(toCurrency) * 100) / 100);
   }
 
   return (
     <form className="sel-form" onSubmit={(e) => e.preventDefault()}>
-    <div className="App">
-    <header className="App-header">
-        <h1>Конвертер валют</h1>
-      </header>
-      <p> </p>
-      <MyCurrency label='Выберите валюту, из которой конвертируем' value={fromCurrency} change={(e) => setFrom(e.target.value)} />
-      <p> </p>
-      <MyCurrency label='Выберите валюту, в которую конвертируем' value={toCurrency} change={(e) => setTo(e.target.value)} />
-      <p> </p>
-      <label>Введите количество исходной валюты:
-          <input type="number" values={amount} onChange={(e) => setAmount(e.target.value)} />
-      </label>
-      <p> </p>
-      <label><button variant="outlined" onClick={CalcResult} type="submit" >Рассчитать</button> Результат = {result}</label>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Конвертер валют</h1>
+        </header>
+        <p> </p>
+        <MyCurrency label='Выберите валюту, из которой конвертируем' value={fromCurrency} change={(e) => setFrom(e.target.value)} />
+        <p> </p>
+        <MyCurrency label='Выберите валюту, в которую конвертируем' value={toCurrency} change={(e) => setTo(e.target.value)} />
+        <p> </p>
+        <label>Введите количество исходной валюты:
+            <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+        </label>
+        <p> </p>
+        <label>
+          <button onClick={calcResult} type="submit" >Рассчитать</button> Результат = {result}
+        </label>
+      </div>
     </form>
   );
 }
